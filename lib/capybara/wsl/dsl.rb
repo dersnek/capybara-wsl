073591e9d1a13d119c67c8ad85165e2ba83c44a7
Mark Tityuk <mark.tityuk@gmail.com>
2020-07-10 19:42:44 +0600

    Pass arguments from DSL-integrated methods

--- lib/capybara/wsl/dsl.rb
+++ lib/capybara/wsl/dsl.rb
@@ -3,12 +3,12 @@
 module Capybara
   module WSL
     module DSL
-      def save_and_open_page_wsl
-        Capybara::WSL.save_and_open_page
+      def save_and_open_page_wsl(path = nil)
+        Capybara::WSL.save_and_open_page(path)
       end
 
-      def save_and_open_screenshot_wsl
-        Capybara::WSL.save_and_open_screenshot
+      def save_and_open_screenshot_wsl(path = nil, **options)
+        Capybara::WSL.save_and_open_screenshot(path, options)
       end
     end
   end
